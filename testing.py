cf1ea48f383c0d2fee1059b618787c3aafe54b00
ravindra <ravindrababu.pilladi541@gmail.com>
2025-09-10 23:56:21 +0530

    vscode-commit

--- testing.py
+++ testing.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-Welcome to the Multicloud class Ravindra -aws01
-=======
-Welcome to the Multicloud class Ravindra
-testing fetch command
-raising merge-conflict
->>>>>>> 61123b5cc3d0cbbbb4b952fdb571c16c7d1f1111
+
+# Welcome to the Multicloud class Ravindra - aws01
+# testing fetch command
+# raising merge-conflict
+now im doing testing fromvscode
+
